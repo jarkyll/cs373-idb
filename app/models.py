c1fcdc462c60bfc50696e1e63d80ff2917dc4f81
Nabeel Virani <nabeelvirani@gmail.com>
2016-07-06 20:36:55 -0500

    added boolean

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Table, ForeignKey, Column, Integer, String
+from sqlalchemy import Table, ForeignKey, Column, Integer, String, Boolean
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
