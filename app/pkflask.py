ae758f2d6fc8a282790ea1fc35b880f73057b396
Pradeep Kumar <pradeep@cs.utexas.edu>
2016-07-10 19:04:16 -0500

    restructuring directory

--- app/pkflask.py
+++ app/pkflask.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 from sqlalchemy import create_engine
@@ -14,10 +14,27 @@ session = DBSession()
 @app.route('/')
 @app.route('/hello')
 def HelloWorld():
-	c = session.query(Character).first()
-	print(c)
-	return '' + c.name
+	return 'home'
 
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
+@app.route('/characters')
+def characters():
+	return render_template('characters.html')
+
+@app.route('/volumes')
+def volumes():
+	return render_template('volumes.html')
+
+@app.route('/teams')
+def teams():
+	return render_template('teams.html')
+
+@app.route('/publishers')
+def publishers():
+	return render_template('publishers.html')
 
 if __name__== "__main__":
 	app.run()
