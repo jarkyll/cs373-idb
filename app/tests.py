a2d9f621598474630d6ecf8ae4a31aa8dcc6b071
Nabeel Virani <nabeelvirani@gmail.com>
2016-07-12 09:05:08 -0500

    wrote one test and the skeleton for the tests

--- app/tests.py
+++ app/tests.py
@@ -10,12 +10,57 @@ class MyTests(TestCase):
 
 
 
-    def test_artist_id(self):
+    def test_publisher_name(self):
         a = Publisher.query.filter_by(name='Image').first()
         self.assertEqual('Image', a.name)
-        #print("Artist Test 1\nExpected: Chon\nActual: "+artist['name'])
 
-    def test_artist_id(self):
+    def test_publisher_image(self):
+
+
+    def test_publisher_city(self):
+
+    def test_publisher_volume(self):
+
+    def test_publisher_characters(self):
+
+
+    def test_publisher_teams(self):
+
+
+    def test_character_name(self):
+
+
+    def test_character_publisher(self):
+
+
+    def test_character_volumes(self):
+
+
+    def test_character_teams(self):
+
+
+    def test_character_image(self):
+
+
+    def test_team_characters(self):
+
+
+    def test_team_publisher(self):
+
+    def test_team_volumes(self):
+
+
+    def test_team_name(self):
+
+    def test_team_image(self):
+
+    def test_volume_name(self):
+
+    def test_volume_publisher(self):
+
+    def test_volume_image(self):
+
+    def test_volume_start_year(self):
 
 if __name__ == "__main__":
     main()
