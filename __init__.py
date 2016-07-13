07d52114ec40c0d80c6d8013ea519e6dd0780cc0
Nabeel Virani <nabeelvirani@gmail.com>
2016-07-13 18:02:10 -0500

    shell can not find files

--- __init__.py
+++ __init__.py
@@ -288,7 +288,7 @@ def homepage():
 @app.route('/runtests', methods=['GET'])
 def runtests():
     try:
-        result = subprocess.check_output(['python3', 'tests.py'])
+        result = subprocess.check_output(['ls'])
         return result
     except Exception as e:
         return str(e)
