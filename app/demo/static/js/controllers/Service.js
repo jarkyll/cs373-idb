7eacf143cb30ba8433f8eee85cac90da1af5d2fc
Pradeep Kumar <pradeep.kumar@utexas.edu>
2016-07-13 18:28:15 -0500

    added api calls for teams, volumes, publishers to return data to angular

--- app/demo/static/js/controllers/Service.js
+++ app/demo/static/js/controllers/Service.js
@@ -23,3 +23,42 @@ app.factory('characters', ['$http', function($http) {
               return err; 
             }); 
 }]);
+
+app.factory('teams', ['$http', function($http) { 
+  return $http.get('http://localhost:5000/api/teams') 
+            .success(function(data) { 
+              console.log('success')
+              console.log(data);
+              return data; 
+            }) 
+            .error(function(err) { 
+              console.log('error')
+              return err; 
+            }); 
+}]);
+
+app.factory('volumes', ['$http', function($http) { 
+  return $http.get('http://localhost:5000/api/volumes') 
+            .success(function(data) { 
+              console.log('success')
+              console.log(data);
+              return data; 
+            }) 
+            .error(function(err) { 
+              console.log('error')
+              return err; 
+            }); 
+}]);
+
+app.factory('publishers', ['$http', function($http) { 
+  return $http.get('http://localhost:5000/api/publishers') 
+            .success(function(data) { 
+              console.log('success')
+              console.log(data);
+              return data; 
+            }) 
+            .error(function(err) { 
+              console.log('error')
+              return err; 
+            }); 
+}]);
