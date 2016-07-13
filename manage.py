8cd9e95d017b04893e7cfaeb35e2cd4f8ec07bbc
Nabeel Virani <nabeelvirani@gmail.com>
2016-07-13 15:10:45 -0500

    fixed single character

--- manage.py
+++ manage.py
@@ -3,7 +3,6 @@ import json
 from json import JSONEncoder
 import os, glob
 import urllib.request
-from unit_models import *
 from app.demo import *
 import jinja2
 from pprint import pprint
@@ -11,48 +10,278 @@ import pickle
 from test_suite import *
 import unittest, sys
 
-try:
-    from StringIO import StringIO
-except ImportError:
-    from io import StringIO
 
-loader = jinja2.FileSystemLoader('templates')
+
 PublisherName = ['Vertigo', 'IDW Publishing', 'Dark Horse Comics', 'Top Cow', 'Valiant', 'Dell', 'Aftershock Comics',
                  'Image', 'Fiction House', 'Boom! Studios']
 dir = os.getcwd() + '/'
 
 
-def jdefault(o):
-    if isinstance(o, set):
-        return list(o)
-    return o.__dict__
+characters_volumes = db.Table('characters_volumes',
+    db.Column('character_name', db.String(150), db.ForeignKey('Character.name')),
+    db.Column('volume_name', db.String(100), db.ForeignKey('Volume.name'))
+)
 
 
-json_example = """
-{
-    "glossary": {
-        "title": "example glossary",
-        "GlossDiv": {
-            "title": "S",
-            "GlossList": {
-                "GlossEntry": {
-                    "ID": "SGML",
-                    "SortAs": "SGML",
-                    "GlossTerm": "Standard Generalized Markup Language",
-                    "Acronym": "SGML",
-                    "Abbrev": "ISO 8879:1986",
-                    "GlossDef": {
-                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
-                        "GlossSeeAlso": ["GML", "XML"]
-                    },
-                    "GlossSee": "markup"
-                }
-            }
+characters_teams = db.Table('characters_teams',
+    db.Column('character_name', db.String(200), db.ForeignKey('Character.name')),
+    db.Column('team_name', db.String(250), db.ForeignKey('Team.name'))
+)
+
+volumes_teams = db.Table('volumes_teams',
+    db.Column('volume_name', db.String(200), db.ForeignKey('Volume.name')),
+    db.Column('team_name', db.String(150), db.ForeignKey('Team.name'))
+)
+
+
+class Character(db.Model):
+    """
+    name: name of characters
+    birth: when the character was born
+    image: image url
+    gender: male or female
+    creator: creator name
+    volumes: volumes that character is in
+    teams: teams that character is in
+    publisher: publisher character is with
+    appear: first appearance
+    real: real name if it exists
+    num_apperances: num of appearnces
+    """
+    __tablename__ = "Character"
+    name = db.Column(db.String(150), unique=True, primary_key=True)
+    birth = db.Column(db.String(100), unique=False)
+    image = db.Column(db.String)  # image url
+    gender = db.Column(db.String(10))
+    creator = db.Column(db.String(50), unique=False)
+
+    appear = db.Column(db.String, unique=False)
+    real = db.Column(db.String, unique=False)
+    num_appearances = db.Column(db.String, unique=False)
+
+    publisher_name = db.Column(db.String(150), db.ForeignKey('Publisher.name'))
+    character_publisher = db.relationship("Publisher", back_populates="publisher_characters")
+
+    character_volumes = db.relationship("Volume", secondary=characters_volumes, back_populates="volume_characters")
+
+    character_teams = db.relationship("Team", secondary=characters_teams, back_populates="team_characters")
+
+    def json_it(self):
+        t = []
+        for team in self.character_teams:
+            t += {'name': team.name, 'image': team.image}
+        v = []
+        for volume in self.character_volumes:
+            v += {'name': volume.name, 'image': volume.image}
+
+        ans = {
+            'name': self.name,
+            'appear': self.appear,
+            'birth': self.birth,
+            'character_publisher': self.character_publisher,
+            'creator': self.creator,
+            'gender': self.gender,
+            'image': self.image,
+            'num_appearances': self.num_appearances,
+            'real': self.real,
+            'character_teams': t,
+            'character_volumes': v
         }
-    }
-}
+        return ans
 
-"""
+    def __repr__(self):
+        return 'Character(name={}, image={}, birth={}, gender={}, creator={}, publisher={}, appear={}, real={}, num_appearances={}'.format(
+            self.name,
+            self.image,
+            self.birth,
+            self.gender,
+            self.creator,
+            self.publisher_name,
+            self.appear,
+            self.real,
+            self.num_appearances
+            ) + ")"
+
+class Volume(db.Model):
+    """
+    image: image url
+    description: info on volume
+    count_of_issues: num issues in volume
+    start_year: when volume started
+    publisher: main publisher
+    characters: characters in volume
+    teams: teams in the characters
+    name: name of volume
+    """
+    __tablename__ = "Volume"
+    image = db.Column(db.String)  # image url
+    description = db.Column(db.String(200), unique=False)
+    start_year = db.Column(db.Integer, unique=False)
+    name = db.Column(db.String, unique=True, primary_key=True)
+    num_issues = db.Column(db.String)
+
+    publisher_name = db.Column(db.String(150), db.ForeignKey('Publisher.name'))
+    volume_publisher = db.relationship("Publisher", back_populates="publisher_volumes")
+
+    volume_characters = db.relationship("Character", secondary='characters_volumes', back_populates="character_volumes")
+
+    volume_teams = db.relationship("Team", secondary=volumes_teams, back_populates='team_volumes')
+
+    def json_it(self):
+        t = []
+        for team in self.volume_teams:
+            temp = {'name': team.name, 'image': team.image}
+            t.append(temp)
+        c = []
+        for character in self.volume_characters:
+            temp = {'name': character.name, 'image': character.image}
+            c.append(temp)
+
+        ans = {
+            'description': self.description,
+            'image': self.image,
+            'name': self.name,
+            'num_issues': self.num_issues,
+            'publisher_name': self.publisher_name,
+            'start_year': self.start_year,
+            'volume_teams': t,
+            'volume_characters': c
+        }
+        return ans
+
+    def __repr__(self):
+        return 'Volume(image={}, description={}, start_year={}, publisher={}, name={}, num_issues={}'.format(
+            self.image,
+            self.description,
+            self.start_year,
+            self.publisher_name,
+            self.name,
+            self.num_issues
+            ) + ")"
+
+
+class Team(db.Model):
+    """
+        name: name of team
+        description: description of team
+        image: image url
+        publisher: main publisher for team
+        characters: characters in team
+        volumes: volumes that team appeard in
+    """
+    __tablename__ = "Team"
+    name = db.Column(db.String(50), unique=True, primary_key=True)
+    description = db.Column(db.String, unique=False)
+    image = db.Column(db.String)  # image url
+    appear = db.Column(db.String, unique=False)
+    num_appearances = db.Column(db.String, unique=False)
+
+    publisher_name = db.Column(db.String(150), db.ForeignKey('Publisher.name'))
+    team_publisher = db.relationship("Publisher", back_populates="publisher_teams")
+
+    team_characters = db.relationship("Character", secondary=characters_teams, back_populates='character_teams')
+    team_volumes = db.relationship("Volume", secondary=volumes_teams, back_populates='volume_teams')
+
+    def __repr__(self):
+        return 'Team(name={}, description={}, image={}, publisher={}, appear={}, num_appearances={}'.format(
+            self.name,
+            self.description,
+            self.image,
+            self.team_publisher,
+            self.appear,
+            self.num_appearances,
+            ) + ")"
+
+    def json_it(self):
+        c = []
+        for char in self.team_characters:
+            temp = {'name': char.name, 'image': char.image}
+            c.append(temp)
+        v = []
+        for volume in self.team_volumes:
+            temp = {'name': volume.name, 'image': volume.image}
+            v.append(temp)
+        ans = {
+            'appear': self.appear,
+            'description': self.description,
+            'image': self.image,
+            'name': self.name,
+            'num_appearances': self.num_appearances,
+            'publisher_name': self.publisher_name,
+            'image': self.image,
+            'num_appearances': self.num_appearances,
+            'team_characters': c,
+            'team_volumes': v
+        }
+        return ans
+
+class Publisher(db.Model):
+    """
+        name: the name of the publisher
+        address: address of publisher if given
+        city: city of publisher
+        state: state of publisher
+        deck: description
+        image: image url
+        characters: character publisher has worked with
+        volumes: volumes publisher has worked with
+        teams: teams publisher has worked with
+    """
+    __tablename__ = "Publisher"
+    name = db.Column(db.String(50), unique=True, primary_key=True)
+    address = db.Column(db.String(100), unique=False)  # maybe this one?
+    city = db.Column(db.String(20), unique=False)
+    state = db.Column(db.String(20), unique=False)
+    deck = db.Column(db.String, unique=False)
+    image = db.Column(db.String)  # image url
+
+    publisher_characters = db.relationship("Character", back_populates="character_publisher")
+
+    publisher_volumes = db.relationship("Volume", back_populates="volume_publisher")
+
+    publisher_teams = db.relationship("Team", back_populates="team_publisher")
+
+    def __repr__(self):
+        return 'Publisher(name={}, address={}, city={}, state={}, deck={}, image={}, publisher_characters={},publisher_volumes={}, publisher_teams={}'.format(
+            self.name,
+            self.address,
+            self.city,
+            self.state,
+            self.deck,
+            self.image,
+            self.publisher_characters,
+            self.publisher_volumes,
+            self.publisher_teams,
+            ) + ")"
+
+    def json_it(self):
+        list_t = []
+        for team in self.publisher_teams:
+            temp = {'name': team.name, 'image': team.image}
+            list_t.append(temp)
+        list_vol = []
+        for volume in self.publisher_volumes:
+            temp = {'name': volume.name, 'image': volume.image}
+            list_vol.append(temp)
+        list_char = []
+        for character in self.publisher_characters:
+            temp = {'name': character.name, 'image': character.image}
+            list_char.append(temp)
+        ans = {
+            'name': self.name,
+            'city': self.city,
+            'deck': self.deck,
+            'address': self.address,
+            'image': self.image,
+            'state': self.state,
+            'publisher_characters': list_char,
+            'publisher_teams': list_t,
+            'publisher_volumes': list_vol
+        }
+        return ans
+
+
+
 
 @app.route("/")
 def homepage():
@@ -216,19 +445,41 @@ def character_api(name):
     real = db.session.query(Character.real).filter_by(name=name).first()
     gender = db.session.query(Character.gender).filter_by(name=name).first()
     publisher_name = db.session.query(Character.publisher_name).filter_by(name=name).first()
+    team_sets = db.session.query(characters_teams).filter_by(character_name=name).all()
+    vol_sets = db.session.query(characters_volumes).filter_by(character_name=name).all()
+    vol_result = []
+    for set in vol_sets:
+        volume = set[1]
+        image = db.session.query(Volume.image).filter_by(name=volume).first()
+        temp = {
+            'name': volume,
+            'image': image
+        }
+        print(temp)
+        vol_result.append(temp)
+    team_result = []
+    for set in team_sets:
+        team = set[1]
+        image = db.session.query(Team.image).filter_by(name=team).first()
+        temp = {
+            'name': team,
+            'image': image
+        }
+        print(temp)
+        team_result.append(temp)
+
     temp = {
         'name': name,
         'real_name': real,
         'image': image,
         'gender': gender,
-        'publisher': publisher_name
+        'publisher': publisher_name,
+        'teams': team_result,
+        'volumes': vol_result
     }
-    print(temp)
+    #print(temp)
     result.append(temp)
     return jsonify({'result': result})
-
-
-    return 'JANOOO'
 
 
 @app.route('/api/publisher/<string:name>', methods=['GET'])
