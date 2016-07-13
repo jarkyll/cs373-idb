a70300fd75ecaf2664e9dc4ba79976f983ccb599
Nabeel Virani <nabeelvirani@gmail.com>
2016-07-13 15:45:55 -0500

    fixing models

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 # from afsiodfajf.database import Base  # afsafsf is going to be our database name
-from app.demo import db
+from cs373-idb import db
 
 characters_volumes = db.Table('characters_volumes',
     db.Column('character_name', db.String(150), db.ForeignKey('Character.name')),
